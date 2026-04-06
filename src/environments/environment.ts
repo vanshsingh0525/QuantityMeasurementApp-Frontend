de6c297d0289d7bd624e3ddd14e988bf1fec3b8d
vanshu0525 <vanshslathia03@gmail.com>
2026-04-06 12:57:45 +0530

    [Vansh] : added Angular Frontend Implementation

---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:8080',
+  googleClientId: '796669911493-bbvjkhcm9a4slbv1nuekf448qkffhfb6.apps.googleusercontent.com'
+};
